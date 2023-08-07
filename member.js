7aaa20261c9aba07439c52f1c30214c8de19dcca
sohaibsyed21 <43613152+sohaibsyed21@users.noreply.github.com>
2023-08-07 02:50:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var skills = document.getElementById("skills");
+    skills.style.display = "none";
+    var projects = document.getElementById("projects");
+    projects.style.display = "none";
+    var about = document.getElementById("about");
+    about.style.display = "none";
+    var contact = document.getElementById("contact");
+    contact.style.display = "none";
+    var home = document.getElementById("home");
+    home.style.display = "none";
+    var blog = document.getElementById("blog");
+    blog.style.display = "none";
+}
